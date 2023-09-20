1e3c8fef93e13d4e444cba469e3e7d999b5f61ba
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-09-20 16:57:56 -0400

    first again

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 import About from "../views/About.vue";
-import AppLayout from "../components/UiComponents/AppLayout.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
-  },
-  {
-    path: "/dashboard",
-    name: "dashboard",
-    meta: { layout: AppLayout },
-    component: () => import("../views/DashboardViews/Dashboard.vue"),
   },
   {
     path: "/login",
@@ -26,12 +19,6 @@ const routes = [
     name: "about",
     component: About,
   },
-  {
-    path: "/board/:boardId",
-    name: "board",
-    meta: { layout: AppLayout },
-    component: () => import("../views/BoardViews/Board.vue"), 
-  }
 ];
 
 const router = createRouter({
