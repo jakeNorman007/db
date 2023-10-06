918d1d6cb078fb7c3c7af6f68ce105574dd56251
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-10-05 23:17:48 -0400

    navbar styling, create modal, delete functions

--- src/components/CreateBoardModal.vue
+++ src/components/CreateBoardModal.vue
@@ -8,15 +8,55 @@ const boardName = ref("");
 </script>
 
 <template>
-  <button @click="modalOpen = true">+ New board</button>
-  <Teleport to="body">
-    <div v-if="modalOpen" class="bg-green-200 rounded-md  fixed z-[999] w-[30rem] h-[35rem] ml-[-150px] left-[45%] top-[15%]">
-      <p>Hello from the modal!</p>
-      <form @submit="createDemoBoard(boardName)">
-          <input type="text" v-model="boardName" />
-          <button type="submit" :disabled="isCreating">Add</button>
-      </form>
-      <button @click="modalOpen = false">Close</button>
-    </div>
-  </Teleport>
+  <button
+    @click="modalOpen = true"
+    class="bg-green-200 px-3 py-3 rounded-md hover:bg-green-300"
+  >
+    + New board
+  </button>
+  <div
+    v-if="modalOpen"
+    @click="modalOpen = false"
+    class="h-screen fixed bg-black z-[998] w-full left-0 top-0 opacity-25"
+  >
+    <Teleport to="body">
+      <div
+        v-if="modalOpen"
+        @click.stop=""
+        class="flex flex-col bg-slate-50 rounded-md fixed z-[999] w-[30rem] h-[20rem] ml-[-150px] left-[45%] top-[15%]"
+      >
+        <p class="px-6 py-[1rem] font-semibold text-xl">Create new board</p>
+        <form @submit="createDemoBoard(boardName)">
+          <div class="my-3">
+            <label class="text-sm text-slate-500 font-semibold px-6"
+              >Board name</label
+            >
+            <input
+              type="text"
+              v-model="boardName"
+              placeholder="e.g. project name..."
+              class="flex px-3 rounded-md ml-6 py-2 w-[27rem] border border-slate-300 hover:border-green-300"
+            />
+          </div>
+          <div class="mb-3">
+            <label class="text-sm text-slate-500 font-semibold px-6"
+              >Description</label
+            >
+            <input
+              type="text"
+              placeholder="e.g. creating backend for app..."
+              class="flex px-3 rounded-md ml-6 py-2 w-[27rem] border border-slate-300 hover:border-green-300"
+            />
+          </div>
+          <button
+            type="submit"
+            :disabled="isCreating"
+            class="bg-green-200 hover:bg-green-300 ml-6 w-[27rem] rounded-md mt-2 py-2"
+          >
+           Create board 
+          </button>
+        </form>
+      </div>
+    </Teleport>
+  </div>
 </template>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -7,27 +7,37 @@
       <div>
         <li
           @click="$router.push('/')"
-          class="focus:bg-green-300 cursor-pointer font-semibold px-6 py-[1rem] hover:w-[14rem] hover:py-[1rem] hover:bg-green-200 hover:rounded-r-3xl"
+          class="mb-2 focus:bg-green-300 hover:bg-green-300 hover:ease-in-out duration-200 cursor-pointer font-semibold px-6 py-[1rem] w-[13rem] py-[1rem] bg-green-200 rounded-r-3xl
+            hover:w-[15rem]"
         >
           Home
         </li>
         <li
           @click="$router.push('/dashboard')"
-          class="active:bg-green-300 cursor-pointer font-semibold px-6 py-[1rem] hover:w-[14rem] hover:py-[1rem] hover:bg-green-200 hover:rounded-r-3xl"
+          class="mb-2 focus:bg-green-300 hover:bg-green-300 hover:ease-in-out duration-200 cursor-pointer font-semibold px-6 py-[1rem] w-[13rem] py-[1rem] bg-green-200 rounded-r-3xl
+            hover:w-[15rem]"
         >
           Dashboard
         </li>
         <li
           @click="$router.push('/about')"
-          class="font-semibold px-6 py-[1rem] hover:w-[14rem] hover:py-[1rem] hover:bg-green-200 hover:rounded-r-3xl"
+          class="mb-2 focus:bg-green-300 hover:bg-green-300 hover:ease-in-out duration-200 cursor-pointer font-semibold px-6 py-[1rem] w-[13rem] py-[1rem] bg-green-200 rounded-r-3xl
+            hover:w-[15rem]"
         >
           About
         </li>
         <li
-          class="font-semibold px-6 py-[1rem] hover:w-[14rem] hover:py-[1rem] hover:bg-green-200 hover:rounded-r-3xl"
+          class="mb-2 focus:bg-green-300 hover:bg-green-300 hover:ease-in-out duration-200 cursor-pointer font-semibold px-6 py-[1rem] w-[13rem] py-[1rem] bg-green-200 rounded-r-3xl
+            hover:w-[15rem]"
         >
           <i class="devicon-github-original pr-2"></i>
           <a href="https://github.com/jakeNorman007/deskbink">Github</a>
+        </li>
+        <li
+          class="mb-2 focus:bg-green-300 hover:bg-green-300 hover:ease-in-out duration-200 cursor-pointer font-semibold px-6 py-[1rem] w-[13rem] py-[1rem] bg-green-200 rounded-r-3xl
+            hover:w-[15rem]"
+        >
+         Settings 
         </li>
       </div>
     </ul>
