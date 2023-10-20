28125165909d245fb8f5ddada02a6015a813d7bc
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-10-19 23:39:27 -0400

    removed draggable

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useCards } from "./CardQueries/useCards";
 import { useGetList } from "../views/ListQueries/useGetList";
-import draggable from "vuedraggable";
+import { format } from "date-fns";
 
 const { list } = useGetList();
 const { cards } = useCards();
@@ -12,23 +12,16 @@ const { cards } = useCards();
     <div
       class="flex flex-col ml-[1rem] font-semibold text-lg w-[50rem] h-[57rem]"
     >
-    <div class="text-3xl p-4">{{ list?.listName }} ({{ cards?.length }})</div>
-      <draggable
-        :list="cards"
-        tag="div"
-        item-key="cardName"
-        @start="drag=true"
-        @end="drag=false"
-        class="overflow-y-scroll scrollbar-hide"
-      >
-        <template #item="{ element: card }">
-          <div
-            class="p-3 m-3 bg-slate-50 h-[12rem] w-[49rem] rounded-md shadow-md shadow-slate-300 border-t-2 border-slate-200"
-          >
-            {{ card?.cardName }}
-          </div>
-        </template>
-      </draggable>
+    {{ list?.listName }} ({{ cards?.length }})
+      <div class="overflow-y-scroll scrollbar-hide">
+        <div
+          v-for="(card, index) in cards"
+          :key="index"
+          class="p-3 m-3 bg-slate-50 h-[12rem] w-[49rem] rounded-md shadow-md shadow-slate-300 border-t-2 border-slate-200"
+        >
+        {{ card.cardName }} {{ format(new Date(card.created_at), "MMM dd yyyy") }}
+        </div>
+      </div>
     </div>
     <div
       class="bg-slate-50 m-3 h-[56rem] rounded-md shadow-md shadow-slate-300 border-t-2 border-slate-200"
